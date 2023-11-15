81f7a5174d0df738ed87f9838b4857ac30df3db5
mlipov <maxim.lipov@gmail.com>
2023-11-15 21:15:50 +0200

    CS-45 Lock with fair guarantee

---
+++ src/telran/multithreading/games/Race.java
@@ -0,0 +1,43 @@
+package telran.multithreading.games;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.concurrent.locks.*;
+
+public class Race {
+	private int distance;
+	private int minSleep;
+	private int maxSleep;
+	Lock lock = new ReentrantLock(true);
+	private ArrayList<Runner> resultsTable;
+	private Instant startTime;
+
+	public Race(int distance, int minSleep, int maxSleep, ArrayList<Runner> resultsTable, Instant startTime) {
+		this.distance = distance;
+		this.minSleep = minSleep;
+		this.maxSleep = maxSleep;
+		this.resultsTable = resultsTable;
+		this.startTime = startTime;
+	}
+
+	public ArrayList<Runner> getResultsTable() {
+		return resultsTable;
+	}
+
+	public Instant getStartTime() {
+		return startTime;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public int getMinSleep() {
+		return minSleep;
+	}
+
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+
+}
---
+++ src/telran/multithreading/games/RaceAppl.java
@@ -0,0 +1,70 @@
+package telran.multithreading.games;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+import telran.view.*;
+
+public class RaceAppl {
+
+	private static final int MAX_THREADS = 10;
+	private static final int MIN_THREADS = 3;
+	private static final int MIN_DISTANCE = 100;
+	private static final int MAX_DISTANCE = 3500;
+	private static final int MIN_SLEEP = 2;
+	private static final int MAX_SLEEP = 5;
+
+	public static void main(String[] args) {
+		InputOutput io = new SystemInputOutput();
+		Item[] items = getItems();
+		Menu menu = new Menu("Race Game", items);
+		menu.perform(io);
+	}
+
+	private static Item[] getItems() {
+		Item[] res = { Item.of("Start new game", RaceAppl::startGame), Item.exit() };
+		return res;
+	}
+
+	static void startGame(InputOutput io) {
+		int nThreads = io.readInt("Enter number of the runners", "Wrong number of the runners", MIN_THREADS,
+				MAX_THREADS);
+		int distance = io.readInt("Enter distance", "Wrong Distance", MIN_DISTANCE, MAX_DISTANCE);
+		Race race = new Race(distance, MIN_SLEEP, MAX_SLEEP, new ArrayList<Runner>(), Instant.now());
+		Runner[] runners = new Runner[nThreads];
+		startRunners(runners, race);
+		try {
+			joinRunners(runners);
+		} catch (InterruptedException e) {
+		}
+		displayResultsTable(race);
+	}
+
+	private static void displayResultsTable(Race race) {
+		System.out.println("place\tracer number\ttime");
+		ArrayList<Runner> resultsTable = race.getResultsTable();
+		IntStream.range(0, resultsTable.size()).mapToObj(i -> toPrintedString(i, race)).forEach(System.out::println);
+	}
+
+	private static String toPrintedString(int index, Race race) {
+		Runner runner = race.getResultsTable().get(index);
+		return String.format("%3d\t%7d\t\t%d", index + 1, runner.getRunnerId(),
+				ChronoUnit.MILLIS.between(race.getStartTime(), runner.getFinsishTime()));
+	}
+
+	private static void joinRunners(Runner[] runners) throws InterruptedException {
+		for (int i = 0; i < runners.length; i++) {
+			runners[i].join();
+		}
+	}
+
+	private static void startRunners(Runner[] runners, Race race) {
+		for (int i = 0; i < runners.length; i++) {
+			runners[i] = new Runner(race, i + 1);
+			runners[i].start();
+		}
+	}
+
+}
