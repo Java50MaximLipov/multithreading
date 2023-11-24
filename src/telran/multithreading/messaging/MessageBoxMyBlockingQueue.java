03f690a91636c46c5464f15c770b9f75efca5c19
mlipov <maxim.lipov@gmail.com>
2023-11-24 09:02:21 +0200

    231121 CW-47 TODO's comments for HW-47

--- src/telran/multithreading/messaging/MessageBoxMyBlockingQueue.java
+++ src/telran/multithreading/messaging/MessageBoxMyBlockingQueue.java
@@ -5,14 +5,15 @@ import java.util.concurrent.*;
 /**
  * Message box contains only one string
  */
-public class MessageBoxBlockingQueue implements MessageBox {
-	private BlockingQueue<String> messages = new LinkedBlockingQueue<>();
+public class MessageBoxMyBlockingQueue implements MessageBox {
+	private MyBlockingQueue<String> messages = new MyLinkedBlockingQueue<>(1);
 
 	@Override
 	public void put(String message) {
 		try {
 			messages.put(message);
 		} catch (InterruptedException e) {
+
 		}
 	}
 
